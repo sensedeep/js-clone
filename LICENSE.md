a315d856bf9322a2893dc8725e2571b482949434
Michael O'Brien <mob@emobrien.com>
2017-05-30 09:25:52 -0700

    DEV: Initial commit

---
+++ LICENSE.md
@@ -0,0 +1,4 @@
+js-blend License
+===
+
+[GPL](http://www.gnu.org/licenses/gpl-2.0.html)
---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+    js-clone - Clone objects and arrays 
+ */
+
+export function clone(src) {
+    var result
+
+    if (Array.isArray(src)) {
+        result = src.slice(0)
+    } else if (typeof src == 'object' && !(src instanceof Date || src instanceof RegExp || src == null)) {
+        result = Object.create(Object.getPrototypeOf(src))
+        var i, descriptor, keys = Object.getOwnPropertyNames(src)
+        for (i = 0; i < keys.length; i ++) {
+            descriptor = Object.getOwnPropertyDescriptor(src, keys[i])
+            if (descriptor.value && typeof descriptor.value === 'object' && keys[i] != '__observers__') {
+                descriptor.value = clone(descriptor.value)
+            }
+            Object.defineProperty(result, keys[i], descriptor)
+        }
+    } else {
+        result = src
+    }
+    return result
+}
