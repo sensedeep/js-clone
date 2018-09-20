8fc1c148d583528323512aaee07b526c654e6f44
Michael O'Brien <mob@emobrien.com>
2018-09-20 16:42:11 +1000

    DEV

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 /*
     js-clone - Clone anything
+
+    Cannot use Object.assign as it is not recursive
  */
 
 const RECURSE_LIMIT = 75
 
 export function clone(src, recurse = 0) {
-    var result
+    let result
 
     if (recurse > RECURSE_LIMIT) {
         return
@@ -17,7 +19,16 @@ export function clone(src, recurse = 0) {
         var i, descriptor, keys = Object.getOwnPropertyNames(src)
         for (i = 0; i < keys.length; i ++) {
             descriptor = Object.getOwnPropertyDescriptor(src, keys[i])
-            if (descriptor.value && typeof descriptor.value === 'object' && keys[i] != '__observers__') {
+            if (descriptor.get) {
+                let value = descriptor.get()
+                if (value === undefined) continue
+                descriptor = {
+                    value,
+                    configurable: descriptor.configurable,
+                    enumerable: descriptor.enumerable,
+                    writable: true,
+                }
+            } else if (descriptor.value && typeof descriptor.value === 'object' && keys[i] != '__observers__') {
                 descriptor.value = clone(descriptor.value)
             }
             Object.defineProperty(result, keys[i], descriptor)
